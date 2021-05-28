be8a1ddff2eb576540e83fcdd546178c90b5a992
welopez <57297784+welopez@users.noreply.github.com>
2021-05-28 20:13:35 -0300

    Agregado de la funcionalidad enviar imagenes

--- app/src/main/java/com/ppc_tp3_lopez/maquetado/Pantalla3.java
+++ app/src/main/java/com/ppc_tp3_lopez/maquetado/Pantalla3.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,6 +25,16 @@ public class Pantalla3 extends AppCompatActivity {
     View layoutFotos;
     ImageView img1;
     Button btnEnviar1;
+    ImageView img2;
+    Button btnEnviar2;
+    ImageView img3;
+    Button btnEnviar3;
+    ImageView img4;
+    Button btnEnviar4;
+    ImageView img5;
+    Button btnEnviar5;
+    ImageView img6;
+    Button btnEnviar6;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -35,42 +49,85 @@ public class Pantalla3 extends AppCompatActivity {
         btnEnviar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uriImagen = Uri.parse("android.resource://" + getPackageName() +"/"+R.drawable.img2);
-                compartirImg(uriImagen);
+                compartirImg(img1);
             }
+        });
 
+        img2 = (ImageView) findViewById(R.id.img2);
+        btnEnviar2 = (Button) findViewById(R.id.btnEnviar2);
+        btnEnviar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compartirImg(img2);
+            }
+        });
 
-//            @Override
-//            public void onClick(View v) {
-//                img1.buildDrawingCache();
-//                Bitmap bitmap = img1.getDrawingCache();
-//
-//                /***** COMPARTIR IMAGEN *****/
-//                try {
-//                    File file = new File(getCacheDir(), bitmap + ".png");
-//                    FileOutputStream fOut = null;
-//                    fOut = new FileOutputStream(file);
-//                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-//                    fOut.flush();
-//                    fOut.close();
-//                    file.setReadable(true, false);
-//                    final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-//                    intent.setType("image/png");
-//                    startActivity(intent);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
+        img3 = (ImageView) findViewById(R.id.img3);
+        btnEnviar3 = (Button) findViewById(R.id.btnEnviar3);
+        btnEnviar3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compartirImg(img3);
+            }
+        });
+
+        img4 = (ImageView) findViewById(R.id.img4);
+        btnEnviar4 = (Button) findViewById(R.id.btnEnviar4);
+        btnEnviar4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compartirImg(img4);
+            }
+        });
+
+        img5 = (ImageView) findViewById(R.id.img5);
+        btnEnviar5 = (Button) findViewById(R.id.btnEnviar5);
+        btnEnviar5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compartirImg(img5);
+            }
+        });
+
+        img6 = (ImageView) findViewById(R.id.img6);
+        btnEnviar6 = (Button) findViewById(R.id.btnEnviar6);
+        btnEnviar6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compartirImg(img6);
+            }
         });
     }
-    private void compartirImg(Uri uriImg){
-        Intent shareIntent = new Intent();
-        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.setType("image/*");
-        shareIntent.putExtra(Intent.EXTRA_STREAM, uriImg);
+
+    private void compartirImg(ImageView imageView){
+
+        if(imageView == null){
+            throw new RuntimeException();
+        }
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        File file = new File(getExternalCacheDir()+"/ppc_tp3_lopez.png");
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        try{
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+            outputStream.flush();
+            outputStream.close();
+
+            shareIntent.setType("image/*");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        } catch (Exception e) {
+            Snackbar.make(findViewById(R.id.layoutFotos), R.string.msgErrorEnvioImg, Snackbar.LENGTH_SHORT).show();
+        }
 
         startActivity(shareIntent);
     }
--- app/src/main/java/com/ppc_tp3_lopez/maquetado/Pantalla5.java
+++ app/src/main/java/com/ppc_tp3_lopez/maquetado/Pantalla5.java
@@ -3,11 +3,22 @@ package com.ppc_tp3_lopez.maquetado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Pantalla5 extends AppCompatActivity {
 
@@ -18,22 +29,46 @@ public class Pantalla5 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla5);
 
+        img7 = (ImageView) findViewById(R.id.img7);
+
         btnEnviar7 = (Button) findViewById(R.id.btnEnviar7);
         btnEnviar7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uriImagen = Uri.parse("android.resource://" + getPackageName() +"/"+R.drawable.img7);
-                compartirImg(uriImagen);
+                compartirImg(img7);
             }
         });
     }
 
-    private void compartirImg(Uri uriImg){
-        Intent shareIntent = new Intent();
-        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.setType("image/*");
-        shareIntent.putExtra(Intent.EXTRA_STREAM, uriImg);
+    private void compartirImg(ImageView imageView){
+
+        if(imageView == null){
+            throw new RuntimeException();
+        }
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        File file = new File(getExternalCacheDir()+"/ppc_tp3_lopez.png");
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        try{
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+            outputStream.flush();
+            outputStream.close();
+
+            shareIntent.setType("image/*");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        } catch (Exception e) {
+            Snackbar.make(findViewById(R.id.layoutPantalla5), R.string.msgErrorEnvioImg, Snackbar.LENGTH_SHORT).show();
+        }
 
         startActivity(shareIntent);
     }
